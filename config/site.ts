b3482f782d7381e8ee5ebfa35b9531cf3f33283e
Soni Dhruv <dhruvrsoni04@gmail.com>
2024-06-11 18:38:25 +0530

    init my portfolio 🚀

---
+++ config/site.ts
@@ -0,0 +1,60 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Next.js + NextUI",
+  description: "Make beautiful websites regardless of your design experience.",
+  navItems: [
+    {
+      label: "About",
+      href: "#about",
+    },
+    {
+      label: "Skills",
+      href: "#Skills",
+    },
+    {
+      label: "Projects",
+      href: "#projects  ",
+    },
+    {
+      label: "Blog",
+      href: "#blog",
+    },
+    {
+      label: "Contact",
+      href: "#contact",
+    },
+  ],
+  navMenuItems: [
+    {
+      label: "About",
+      href: "#about",
+    },
+    {
+      label: "Skills",
+      href: "#Skills",
+    },
+    {
+      label: "Projects",
+      href: "#projects",
+    },
+    {
+      label: "Team",
+      href: "#team",
+    },
+    {
+      label: "Blog",
+      href: "#blog",
+    },
+    {
+      label: "Contact",
+      href: "#contact",
+    },
+  ],
+  links: {
+    github: "https://github.com/dhruv8433",
+    twitter: "https://x.com/dhruv_s_o_n_i",
+    docs: "https://nextui.org",
+    discord: "https://discord.com/channels/dhruv7143",
+  },
+};
